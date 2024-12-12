4c4fcb916c5acc4745da46d3da8cc371e5b6c17f
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2024-12-12 19:08:38 +0530

    working sample home page

---
+++ src/routes/question/add/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  /**
+   * @type {string[]}
+   */
+  let answers = $state([]);
+  let tmp_answer = $state("");
+
+  let add = () => {
+    console.log("clicked");
+    answers.push(tmp_answer);
+  };
+
+  let remove = (index) => {
+    answers.splice(index, 1);
+  };
+</script>
+
+<h1>Add Question</h1>
+
+<textarea id="question"></textarea>
+<br />
+{#each answers as answer, i}
+  <input type="text" bind:value={answers[i]} /><button onclick={() => remove(i)}
+    >Delete</button
+  >
+  <br />
+{/each}
+<input type="text" bind:value={tmp_answer} />
+<button onclick={add}>add</button>
+<br />
+<button onclick={() => console.log($state.snapshot(answers))}>submit</button>
