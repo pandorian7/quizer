4960e64356b091b267414afd0093d39810485e4a
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2024-12-14 13:09:26 +0530

    now it redirects to the relvent question edit page after adding a question

--- src/routes/question/add/+page.js
+++ src/routes/question/add/+page.js
@@ -1,15 +1,19 @@
+import { goto } from "$app/navigation";
+
 export function load() {
   const update = async (question, answers) => {
-    const { id } = question;
-    await fetch(`/api/questions/`, {
+    // const { id } = question;
+    let res = await fetch(`/api/questions/`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ question, answers }),
     });
+    let { id } = await res.json();
+    goto(`/question/${id}`);
   };
   return {
     editing: false,
-    question: { id: null, question: "" },
+    question: { id: null, question: "", multiple_answers: 0 },
     answers: [],
     update,
   };
