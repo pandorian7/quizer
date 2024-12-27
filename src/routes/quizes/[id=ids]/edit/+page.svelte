4730a8e34d866862b5ee4d66543c9e0f7f107605
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2024-12-27 13:01:54 +0530

    functionally complete quiz edit page

--- src/routes/quizes/[id=ids]/edit/+page.svelte
+++ src/routes/quizes/[id=ids]/edit/+page.svelte
@@ -6,14 +6,17 @@
   import QuestoinBox from "$lib/Components/QuestoinBox.svelte";
   import { validateQuestionandAnswers } from "$lib/quizer.js";
   import quizer from "$lib/quizer.js";
+  import MessageBox from "$lib/Components/MessageBox.svelte";
 
   import { slide } from "svelte/transition";
   import Loading from "$lib/Components/Loading.svelte";
+  import { invalidate } from "$app/navigation";
 
   const { data } = $props();
 
   let quiz = $state(data.quiz);
   let loader;
+  let msgbox;
 
   $effect(() => {
     // on reload this will make sure that the data is updated
@@ -46,18 +49,63 @@
       }
       return ok;
     },
-    save: () => {
+    save: async () => {
       const ok = on.verify();
-      if (!ok) return;
-      quizer.quizes.update(quiz.id, quiz.title);
-      // console.log($state.snapshot(quiz));
+      if (!ok) {
+        msgbox.danger("Please fix the errors");
+        return;
+      }
+
+      const questionsTo = {
+        add: quiz.questions.filter((q) => q.id === null),
+        update: quiz.questions.filter((q) => q.id !== null),
+        delete: data.quiz.questions.filter(
+          (qOrg) => quiz.questions.findIndex((q) => q.id === qOrg.id) === -1
+        ),
+      };
+
+      const tasks = [
+        quizer.quizes.update(quiz.id, quiz.title),
+        ...questionsTo.add.map((q) => {
+          const { question, answers, multiple_answers } = q;
+          return quizer.questions.add(
+            { question, multiple_answers },
+            $state.snapshot(answers),
+            quiz.id
+          );
+        }),
+        ...questionsTo.delete.map((q) => quizer.questions.remove(q.id)),
+        ...questionsTo.update.map((q) => {
+          const { question, answers, multiple_answers } = q;
+          return quizer.questions.update(
+            { question, multiple_answers },
+            $state.snapshot(answers),
+            q.id
+          );
+        }),
+      ];
+
+      const status = await loader.loading(() => Promise.allSettled(tasks));
+
+      const nResolved = status.reduce(
+        (prv, { status }) => (status == "fulfilled" ? prv + 1 : prv),
+        0
+      );
+      if (nResolved == tasks.length) {
+        msgbox.success("Quiz saved");
+      } else {
+        msgbox.warning(
+          `Error: ${tasks.length - nResolved} of ${tasks.length} failed`
+        );
+      }
+      invalidate(`/api/quizes/${quiz.id}`);
     },
-    add: (question) => console.log(),
   };
 </script>
 
+<MessageBox bind:this={msgbox} />
 <Loading bind:this={loader} />
-<Dial onclick={() => loader.loading(on.save)} text="Save" />
+<Dial onclick={on.save} text="Save" />
 <div style:margin-top="2rem">
   <Container>
     <Card --card-background="var(--grey-700)" --card-padding="0">
