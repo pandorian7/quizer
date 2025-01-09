c746b4b1b1c9189f93a59b73e42d76e61304f527
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-01-09 15:47:27 +0530

    /quize/{id} send user to either attempt or edit depending on the ownership

---
+++ src/routes/quizes/[id=ids]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  const { data } = $props();
+  const prefix = `/quizes/${data.quiz.id}/`;
+
+  onMount(() => {
+    if (data.user.id === data.quiz.owner_id) {
+      goto(prefix + "edit");
+    } else {
+      goto(prefix + "attempt");
+    }
+  });
+</script>
